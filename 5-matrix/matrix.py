561a67a946c51a150c66755817c0e60b1218c17e
andreapas79 <andreapas79@gmail.com>
2020-03-22 17:31:13 +0100

    My first solution

--- 5-matrix/matrix.py
+++ 5-matrix/matrix.py
@@ -1,9 +1,17 @@
 class Matrix:
+
     def __init__(self, matrix_string):
-        pass
+        m = matrix_string.split('\n')
+        mat = []
+        for i in range(0, len(m)):
+            mat.append([int(a) for a in (m[i].split())])
+        self.mat = mat
 
     def row(self, index):
-        pass
+        return self.mat[index-1]
 
     def column(self, index):
-        pass
+        ret = []
+        for i in range(0, len(self.mat)):
+            ret.append(self.mat[i][index-1])
+        return ret
